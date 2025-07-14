ca94a9551715c11d21f71927d616ada34609f476
Quan Do <quando952@gmail.com>
2025-07-14 16:57:20 +0700

    update socket FE

--- Client/src/contexts/SocketContext.jsx
+++ Client/src/contexts/SocketContext.jsx
@@ -7,15 +7,25 @@ const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   const SERVER_URL = 'https://datn-quanlinhathuoc.onrender.com';
+
   useEffect(() => {
-    const socketIntance = io(SERVER_URL);
-    setSocket(socketIntance);
+    const socketInstance = io(SERVER_URL, {
+      transports: ['websocket'], 
+      withCredentials: true      
+    });
+
+    setSocket(socketInstance);
 
     return () => {
-      socketIntance.disconnect();
+      socketInstance.disconnect();
     };
   }, []);
-  return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
 
 export const useSocket = () => {
